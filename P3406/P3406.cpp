9e1c24a424f311524b7d244cbe2bb2d8350b2a49
Tan_Zhixia <pxk_summer@126.com>
2025-06-03 21:01:37 +0800

    git

--- P3406/P3406.cpp
+++ P3406/P3406.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
-#include <cstring>
+#define int long long
 using namespace std;
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+signed main() {
     int n, m;
-    int a[100005], diff[100005];
-    memset(diff, 0, sizeof(diff));
     cin >> n >> m;
+    int a[100005], d[100005];
+    for (int i = 1; i <= 100000; i ++)
+        d[i] = 0;
     int last;
     cin >> last;
     m --;
     while (m --) {
         int to;
         cin >> to;
-        last;
+        if (to < last) {
+            d[to] ++;
+            if (last <= n)
+                d[last] --;
+            last = to;
+            continue;
+        }
+        d[last] ++;
+        if (to <= n)
+            d[to] --;
+        last = to;
     }
+    a[0] = 0;
+    for (int i = 1; i <= n; i ++)
+        a[i] = a[i - 1] + d[i];
+    int ans = 0;
+    for (int i = 1; i <= n - 1; i ++) {
+        int _a, b, c;
+        cin >> _a >> b >> c;
+        ans += min(_a * a[i], b * a[i] + c);
+    }
+    cout << ans << endl;
+    return 0;
 }
